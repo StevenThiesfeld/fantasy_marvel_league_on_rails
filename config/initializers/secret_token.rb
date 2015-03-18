5fed45f8bb7db6ffdefa8b57076370d91d7b9d32
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-03-18 14:58:35 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FantasyMarvelLeagueOnRails::Application.config.secret_token = '27d0bdd8a8e920ebfd1b22698b72bb5c88364d62e3345bdae8a6d742a3b4cd9409b85fe0e2ace75530e477e60286c2b83c9a4955d8219eed7ff14485024e87e4'
